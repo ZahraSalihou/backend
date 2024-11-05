64604bc82fa4924ace99be6bff7593f0c7b42d44
ZahraSalihou <fatoucha.diallo10@gmail.com>
2024-11-05 20:37:27 +0100

    second

--- app.js
+++ app.js
@@ -1,24 +1,75 @@
+
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Thing = require('./models/thing');
+const app = express();mongoose.connect('mongodb+srv://fatouchadiallo10:toUOJ1nwg6bHa6C6@cluster0.48ieu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((error) => console.error('Connexion à MongoDB échouée :', error));
 
-const app = express();
+app.use(bodyParser.json());
+
+// La méthode app.use() vous permet d'attribuer un middleware à une route spécifique de votre application.
+
+// Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
+
+// Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse.
+
+// Les méthodes de votre modèle Thing permettent d'interagir avec la base de données :
+
+// save()  – enregistre un Thing ;
+
+// find()  – retourne tous les Things ;
+
+// findOne()  – retourne un seul Thing basé sur la fonction de comparaison qu'on lui passe (souvent pour récupérer un Thing par son identifiant unique).
+
+// La méthode  app.get()  permet de réagir uniquement aux requêtes de type GET.
+// app.put()  et  app.delete()  attribuent des middlewares aux requêtes de type PUT et de type DELETE.
+
+// Les méthodes  updateOne()  et  delete()  de votre modèle Thing permettent de mettre à jour ou de supprimer un Thing dans la base de données.
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+// En passant votre middleware à  app.post()  au lieu de  app.use()  , il répondra uniquement aux requêtes de type POST.
+// app.post('/api/stuff', (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: 'Objet créé !'
+//   });
+// });
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
